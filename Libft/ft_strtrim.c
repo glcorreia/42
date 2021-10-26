e6b5451b83e2ffe8e6d861595a911c3ca86763ee
Goncalo Correia <gonkbsd@gmail.com>
2021-10-26 15:52:33 +0100

    Remove obsolete function

--- Libft/ft_strtrim.c
+++ Libft/ft_strtrim.c
@@ -12,32 +12,7 @@
 
 #include "libft.h"
 
-/*	x correr str 1 a 1
-**	x a cada iteraccao correr while de set. => funcao: static int check_set(*str, *set) return 1/0
-**	x quando deixar de encontrar, define start pos
-**	correr 1 a 1 do fim para start pos
-**	a cada iteraccao correr while de set. => funcao: static int check_set(*str, *set) return 1/0
-**	quando deixar de encontrar, define end pos
-**	alocar memoria para len = end - start
-**	popular new_str de start a end.
-**	return new_str
-*/
-
-static int	check_start(const char *s1, const char *set)
-{
-	size_t	i;
-
-	i = 0;
-	while(i < ft_strlen(set))
-	{
-		if (*s1 == set[i])
-			return (1);
-		i++;
-	}
-	return (0);
-}
-
-static int	check_end(const char *s1, const char *set)
+static int	check_set(const char *s1, const char *set)
 {
 	size_t	i;
 
@@ -58,13 +33,13 @@ char	*ft_strtrim(char const *s1, char const *set)
 	char	*result;
 
 	start = 0;
-	while(ft_strlen(s1) && check_start(s1, set) == 1) // CHECK START POS
+	while(ft_strlen(s1) && check_set(s1, set) == 1) // CHECK START POS
 	{
 		start++;
 		s1++;
 	}
 	end = ft_strlen(s1) - 1;	
-	while(s1[end] && check_end((char *)&s1[end], set) == 1) // CHECK END POS
+	while(s1[end] && check_set((char *)&s1[end], set) == 1) // CHECK END POS
 		end--;
 	start = 0;
 	result = (char *)malloc(sizeof(char) * (end + 1) + 1);
@@ -75,8 +50,6 @@ char	*ft_strtrim(char const *s1, char const *set)
 	result[start] = '\0';
 	return (result);
 }
-
-
 
 int	main(void)
 {
